4584d317a8785b03e1ada3e4bfb950aabd10f55a
adeshpande@rubiconproject.com <deshpana@usc.edu>
2016-09-22 09:42:35 -0700

    Removed the submission scripts

--- SubmissionScripts/CSCI567_hw1_fall16.py
+++
@@ -1,6 +0,0 @@
-import NaiveBayes
-import KNearestNeighbours
-
-if __name__ == "__main__":
-    NaiveBayes.print_naivebayes_accuracy('train.txt', 'test.txt')
-    KNearestNeighbours.print_knn_accuracy('train.txt', 'test.txt')
